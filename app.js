2872f6e760d1ef80737d0f48991bd229a69dec36
Emil Larsson <emil.larsson@navigaglobal.com>
2022-09-26 08:13:06 +0200

    Update example and remove console.log

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ import {
   convertToClass
 } from './lib/helperFunctions.js'
 
-console.log('CSV To XML converter')
-
 /*
 Filformat input:
 P|förnamn|efternamn
@@ -29,21 +27,15 @@ A|White House|Washington, D.C
 */
 
 const str = `
-P|Victoria|Bernadotte
-T|070-0101010|0459-123456
-A|Haga Slott|Stockholm|101
-F|Estelle|2012
-A|Solliden|Öland|10002
-F|Oscar|2016
-T|0702-020202|02-202020
-P|Joe|Biden
-A|White House|Washington, D.C
-P|Emil|Larsson
-A|Adelgatan|Kalmar
-F|Edwin|2014
-A|Adelgatan|Kalmar
-F|Alma|2016
-T|0702-020202|02-202020
+P|Carl Gustaf|Bernadotte
+T|0768-101801|08-101801
+A|Drottningholms slott|Stockholm|10001
+F|Victoria|1977
+A|Haga Slott|Stockholm|10002
+F|Carl Philip|1979
+T|0768-101802|08-101802
+P|Barack|Obama
+A|1600 Pennsylvania Avenue|Washington, D.C
 `
 
 const result = groupInputToSeperatePersons(str.split('\n'))
